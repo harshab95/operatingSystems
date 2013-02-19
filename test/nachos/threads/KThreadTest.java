3c832f8f6588ba93d2eede4d4bfaafe8a8e3e997
Japheth <japhethwong@ubuntu.(none)>
2013-02-18 16:41:02 -0800

    Changed project structure: moved all code under src folder; created test folder which will contain our JUnit tests.

---
+++ test/nachos/threads/KThreadTest.java
@@ -0,0 +1,14 @@
+package nachos.threads;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class KThreadTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
